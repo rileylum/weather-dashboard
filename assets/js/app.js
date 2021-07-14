38eb397745c68b42fa716c1024a2b8eb1f9b4d41
Riley Lum <rileylum@adam.com.au>
2021-07-14 20:54:44 +0930

    create api calls to get current weather data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,34 @@
 
-var fakeData = {
-    "lat": 33.44,
-    "lon": -94.04,
-    "timezone": "America/Chicago",
-    "timezone_offset": -21600,
-    "current": {
-        "dt": 1618317040,
-        "sunrise": 1618282134,
-        "sunset": 1618333901,
-        "temp": 284.07,
-        "feels_like": 282.84,
-        "pressure": 1019,
-        "humidity": 62,
-        "dew_point": 277.08,
-        "uvi": 0.89,
-        "clouds": 0,
-        "visibility": 10000,
-        "wind_speed": 6,
-        "wind_deg": 300,
-        "weather": [
-            {
-                "id": 500,
-                "main": "Rain",
-                "description": "light rain",
-                "icon": "10d"
-            }
-        ]
-    }
+var cityName = 'adelaide'
+var apiKey = '8d20771314feba21a1dc624717e99f62'
+
+async function getCityLatLong(city) {
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    const response = await fetch(requestUrl);
+    const predict = await response.json();
+    return predict;
 };
+
+async function getWeatherData(coords) {
+    var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coords.coord.lat + "&lon=" + coords.coord.lon + "&appid=" + apiKey;
+    const response = await fetch(requestUrl);
+    const predict = await response.json();
+    return predict;
+};
+
+function getData() {
+    getCityLatLong(cityName)
+        .then(function (coords) {
+            getWeatherData(coords)
+                .then(function (data) {
+                    console.log(data);
+                    fillCurrentForecast(data);
+                });
+        });
+};
+
+
+getData();
 
 function createCurrentForecast() {
     var currentForecastElem = document.createElement('div');
@@ -51,9 +52,9 @@ function createCurrentForecast() {
     currentForecastElem.appendChild(uvIndexElem);
 
     document.querySelector('#forecast').appendChild(currentForecastElem);
-}
+};
 
-function fillCurrentForecast(cityName, data) {
+function fillCurrentForecast(data) {
 
     var headerElem = document.querySelector('#header');
     var tempElem = document.querySelector('#temp');
@@ -61,16 +62,15 @@ function fillCurrentForecast(cityName, data) {
     var humidityElem = document.querySelector('#humidity');
     var uvIndexElem = document.querySelector('#uv');
 
-    headerElem.textContent = cityName + data.current.dt + data.current.weather[0].main;
+    headerElem.textContent = "cityName" + data.current.dt + data.current.weather[0].main;
     tempElem.textContent = data.current.temp;
     windElem.textContent = data.current.wind_speed;
     humidityElem.textContent = data.current.humidity;
     uvIndexElem.textContent = data.current.uvi;
-}
+};
 
 
 createCurrentForecast();
-fillCurrentForecast('Texarkana', fakeData);
-// example get request for adelaide weather
-// fetch('https://api.openweathermap.org/data/2.5/weather?q=adelaide&appid=8d20771314feba21a1dc624717e99f62').then(function(response){return response.json();}).then(function(data){console.log(data);})
+
+
 
